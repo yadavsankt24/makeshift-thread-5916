8b3887b9bf3cc0a2299811c9f9cee1b16f621b8d
sanghamitra0591 <sanghamitramymail@gmail.com>
2022-11-12 13:44:30 +0530

    added nav

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import {Routes,Route} from "react-router-dom"
-import Homepage from '../Components/Homepage/Homepage'
 import SignUp from './SignUp'
-
+import Login from './Login'
 const AllRoutes = () => 
 {
   return (
     <>
     <Routes>
-    <Route path = "/" element = {<SignUp/>} />
+            <Route path = "/" element = {<SignUp/>} />
             <Route path = "/login" element = {<Login />} />
             {/* <Route path = "/signAuth" element = {<SignUp/>} /> */}
     </Routes>
