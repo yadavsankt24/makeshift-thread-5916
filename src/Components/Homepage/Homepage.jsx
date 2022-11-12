802460e9726a74cacce0891205a04a11558d12bb
sanghamitra0591 <sanghamitramymail@gmail.com>
2022-11-12 13:37:49 +0530

    added nav

---
+++ src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+
+
+function Homepage(){
+    return <Box>
+        Home Page
+    </Box>
+}
+
+export default Homepage;
--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Routes,Route} from "react-router-dom"
+import Homepage from '../Components/Homepage/Homepage'
 import SignUp from './SignUp'
 
 const AllRoutes = () => 
@@ -7,9 +8,9 @@ const AllRoutes = () =>
   return (
     <>
     <Routes>
-            <Route path = "/" element = {<SignUp/>} />
+            <Route path = "/" element = {<Homepage />} />
 
-            {/* <Route path = "/signAuth" element = {<SignUp/>} /> */}
+            <Route path = "/signAuth" element = {<SignUp/>} />
     </Routes>
    </>
   )
