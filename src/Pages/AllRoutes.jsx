ccdc19752fb6b6b34252cf1fe9f5c09edbba9a4a
apoorvachikara <chikara02apoorva@gmail.com>
2022-12-09 22:03:55 +0530

    Remove commented components

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,3 +1,4 @@
+// feedback: fw20_0591, fw17_0058 and fw20_0209 removed commented components
 // import React from 'react'
 // import {Routes,Route} from "react-router-dom"
 // import SignUp from './SignUp'
